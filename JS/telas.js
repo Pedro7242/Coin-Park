ca5ae9fb09439cfc3a68e273e7367c90b7da2a1e
JoaoGabriel-16 <joaogabrielvicentevelosoo@gmail.com>
2025-06-11 23:37:49 -0300

    Update telas.js

--- JS/telas.js
+++ JS/telas.js
@@ -1,1 +1,8 @@
-
+window.onload = function(){
+    const nome = localStorage.getItem("nomeJogador")
+    if(!nome){
+        alert("Você precisa escolher um nome!")
+        window.location.href = "PaginaInicial.html"
+        return
+    }
+}
