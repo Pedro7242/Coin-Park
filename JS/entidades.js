2bdf57c196b89eedd874903c32aa490d51893417
JoaoGabriel-16 <joaogabrielvicentevelosoo@gmail.com>
2025-06-16 20:24:38 -0300

    Atualizar o entidades.js

--- JS/entidades.js
+++ JS/entidades.js
@@ -1,1 +1,21 @@
 
+class Moeda {
+  constructor(elementoHtml) {
+    this.elemento = elementoHtml;
+  }
+
+  verificarColisao(jogador) {
+    const retJogador = jogador.getBoundingClientRect();
+    const retMoeda = this.elemento.getBoundingClientRect();
+
+    const colidiu =
+      retJogador.left < retMoeda.right &&
+      retJogador.right > retMoeda.left &&
+      retJogador.top < retMoeda.bottom &&
+      retJogador.bottom > retMoeda.top;
+
+    if (colidiu) {
+      this.elemento.remove(); // Remove a moeda da tela
+    }
+  }
+}
