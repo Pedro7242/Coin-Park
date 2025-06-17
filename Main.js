a454e77b76618b574647ec1fda4cad6191328f45
JoaoGabriel-16 <joaogabrielvicentevelosoo@gmail.com>
2025-06-16 21:02:46 -0300

    Atualizar o Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,16 @@
 
+let moedas = Array.from(document.querySelectorAll('.moeda')).map(el => new Moeda(el));
+const porta = new Porta(document.querySelector('.porta'));
+const jogador = document.querySelector('#jogador');
+
+
+function loopJogo() {
+  verificarColisoesMoedas(jogador);
+  porta.checarEntrada({ el: jogador });
+
+  
+  requestAnimationFrame(loopJogo);
+}
+
+
+loopJogo();
