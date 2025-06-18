75caa3bdb1cafe639b1c16d85428eb66566f4ce4
RlinsC <rhlc1@aluno.ifal.edu.br>
2025-06-18 15:42:34 -0300

    Delete JS directory

--- JS/telas.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    const nome = localStorage.getItem("nomeJogador")
-    if(!nome){
-        alert("Você precisa escolher um nome!")
-        window.location.href = "PaginaInicial.html"
-        return
-    }
-}
